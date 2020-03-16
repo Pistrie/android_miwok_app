325cf028ad563f28c0e5ddef66e7c8673cf20e23
Pistrie <39904003+Pistrie@users.noreply.github.com>
2020-03-16 12:31:27 +0100

    added project to github

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,19 @@
+package com.example.android.miwok;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+    }
+}
